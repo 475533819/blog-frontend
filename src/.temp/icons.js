5094c0ca60de30e6e7ba9d364ceae423ec4c112b
xinchlu <475533819@qq.com>
2021-03-23 20:29:03 +0800

    all

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/favicon.png?width=76&key=d38128f"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/favicon.png?width=152&key=d38128f"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/favicon.png?width=120&key=d38128f"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/favicon.png?width=167&key=d38128f"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/favicon.png?width=180&key=d38128f"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/favicon.png?width=16&key=794f077"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/favicon.png?width=32&key=794f077"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/favicon.png?width=96&key=794f077"
+    }
+  ]
+}
